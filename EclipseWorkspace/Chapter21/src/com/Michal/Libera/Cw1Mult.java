c16c4b6d7c0ad1eaba546527b76bd2e323e60f1c
nevimess <m.w.libera@gmail.com>
2016-08-20 12:02:08 +0200

    test2

--- EclipseWorkspace/Chapter21/src/com/Michal/Libera/Cw1Mult.java
+++ EclipseWorkspace/Chapter21/src/com/Michal/Libera/Cw1Mult.java
@@ -2,4 +2,13 @@ package com.Michal.Libera;
 
 public class Cw1Mult {
 
+	public static void main(String...args){
+		
+		for(int i = 0; i<10; i++){
+			
+			new Thread(new Cw1()).start();
+		}
+	}
+	
+	
 }
